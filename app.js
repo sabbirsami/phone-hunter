fc8bb0eb532e70a18230907887fca9bee0b97e51
SabbirSami <smd71430@gmail.com>
2022-03-01 00:27:28 +0600

    get single phone using forEach done

--- app.js
+++ app.js
@@ -8,3 +8,37 @@ const loadPhone = () => {
     document.getElementById("products").textContent = "";
 };
 loadPhone();
+// get single phone
+const getPhone = (phones) => {
+    phones.forEach((phone) => {
+        // console.log(phone);
+        const productsContainer = document.getElementById("products");
+        const div = document.createElement("div");
+        div.classList.add("col");
+        div.innerHTML = `
+            <div class="card shadow">
+                <div class="product_img p-5">
+                    <img
+                        src="${phone.image}"
+                        class="card-img-top rounded"
+                        alt=""
+                    />
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">Brand: ${phone.brand}</p>
+                    <button
+                        onclick="loadPhoneDetails('${phone.slug}')"
+                        type="button"
+                        class="btn btn-primary"
+                        data-bs-toggle="modal"
+                        data-bs-target="#staticBackdrop"
+                        id="details">
+                        Details
+                    </button>
+                </div>
+            </div>
+        `;
+        productsContainer.appendChild(div);
+    });
+};
